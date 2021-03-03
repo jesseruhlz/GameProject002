0cc17a59fa73d3bfd4bd75bc6cf16955fc7df1ba
jesseruhlz <60628685+jesseruhlz@users.noreply.github.com>
2021-03-02 18:18:49 -0800

    Falling Platform Finish
    Fully implemented the falling platform. Once the player lands on the platform, it will fall for x amount of seconds and then move back up once the player has left the platform. In addition to that, the player will teleport back to the beginning if they stay on the pad while it enters the water.

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 initialPosition;
+    bool platformMovingBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (platformMovingBack)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, initialPosition, 20f * Time.deltaTime);
+        }
+        if (transform.position.y == initialPosition.y)
+        {
+            platformMovingBack = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Player") && !platformMovingBack)
+        {
+            Invoke("DropPlatform", 0.5f);
+        }
+    }
+
+    void DropPlatform()
+    {
+        rb.isKinematic = false;
+        Invoke("GetPlatformBack", 2f);
+    }
+    
+    void GetPlatformBack()
+    {
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        platformMovingBack = true;
+    }
+}

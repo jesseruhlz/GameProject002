5fd19823b9fb9a6d86d34c4018ee81e630eac12b
jesseruhlz <60628685+jesseruhlz@users.noreply.github.com>
2021-03-05 09:55:03 -0800

    Quick Additions
    - Added a rock object in the scene that when the player triggers it, UI will show that player can use rocks
    - Added a new track to the second level

---
+++ Assets/Scripts/RockPickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RockPickup : MonoBehaviour
+{
+    public GameObject UiObject;
+    public GameObject rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+
+       void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            UiObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        UiObject.SetActive(false);
+        Destroy(rock);
+    }
+}

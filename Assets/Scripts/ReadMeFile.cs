c583335ea3771c4e586ae971772a035d5da98109
jesseruhlz <60628685+jesseruhlz@users.noreply.github.com>
2021-03-05 15:59:08 -0800

    Last Touches Before Submission
    - Decided to not implement the raise platform with trigger
    - Fixed know issue with player spawning in the middle of the map in air
    - Added a pause/quit menu

--- Assets/Scripts/ReadMeFile.cs
+++ Assets/Scripts/ReadMeFile.cs
@@ -4,9 +4,28 @@
  * "Unity 2 Player Game Tutorial #4 - Throwing Snowballs". gamesplesjames. https://www.youtube.com/watch?v=lwzUiOCOqVA&list=LLCIRmkP2EUyZSwjAQjObRgQ&index=12
  * "How To Add Step Sounds Effect To Unity Game? Simple Unity 2D Tutorial." .Alexander Zotov. https://www.youtube.com/watch?v=6hBdRpXYNy4&list=LLCIRmkP2EUyZSwjAQjObRgQ&index=11
  * "START MENU in Unity". Brackeys. https://www.youtube.com/watch?v=zc8ac_qUXQY&list=LLCIRmkP2EUyZSwjAQjObRgQ&index=104
+ * "How to easily display text in unity by a trigger! c#". Kalm1 Games. https://www.youtube.com/watch?v=IvHqAzhIDSE&list=LL&index=22
+ * "How to Make Unity 2D Enemy Patrol Movements Unity3D Tutorial". LemauDev. https://www.youtube.com/watch?v=ljQpwxGb7Jc&list=LL&index=28
+ * "Unity 2D Collectiong Coins Tutorial". Xlaugts. https://www.youtube.com/watch?v=DZ-3g31jk90&list=LL&index=47
+ * "Unity 2D Moving PLatform Tutorial". Xlaugts. https://www.youtube.com/watch?v=8aSzWGKiDAM&list=LL&index=71
+ * "Unity2D Tutorial How to Return Platform To Its Initial Position After Falling Moving It Upwards". Alexander Zotov. https://www.youtube.com/watch?v=5sRCTY9wmmY&list=LL&index=49
+ * "How to create a simple 2D Portal | Unity| Easy". AlienDev. https://www.youtube.com/watch?v=Mjw8GGzuwKg&list=LL&index=63
  * 
- * Additions to Game
+ * 
+ * Additions to Game CPSC236
  * 1. Fuctional Menu that leads to game scene 1
  * 2. Added sound effects (background music, footsteps, throwing sound)
  * 3. Added a throw option (F key) with animation and sound
+ * 
+ * Additions to Project 2 CPSC 244
+ * 1. New Platform Types
+ *  - Automatic Horizontal Moving Platform
+ *  - Automatic Vertical Moving Platform
+ *  - Automatic Falling Platform (when player collides on top of it)
+ * 2. New usage mechanic
+ *  - Coin collection: now has UI element that increases amount of gemstones the player has collected
+ * 3. Two New Mechanics
+ *  - Player can shoot a projectile (in the game it is a rock)
+ *  - Patrol Enemy (moves back and fourth between two positions and dissapearing when player projectile collides with it)
+ *
  */
